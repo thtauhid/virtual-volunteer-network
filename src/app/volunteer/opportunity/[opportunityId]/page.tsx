00b5bf5788b9518f206b1681b00a2efc424cd171
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2024-01-25 16:32:28 +0530

    Vol: single opportunity

--- src/app/volunteer/opportunity/[opportunityId]/page.tsx
+++ src/app/volunteer/opportunity/[opportunityId]/page.tsx
@@ -1,22 +1,33 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import { formatDateTime } from "@/lib/time";
 
-const opportunity = {
-  id: 2,
-  title: "Opportunity 2",
-  details: "Details 2",
-  location: "Location 2",
-  start_date: "Start Date 2",
-  end_date: "End Date 2",
-};
+interface Props {
+  params: {
+    opportunityId: string;
+  };
+}
 
-export default function SingleOpportunityPage() {
+export default async function SingleOpportunityPage(props: Props) {
+  const opportunity = await prisma.opportunity.findUnique({
+    where: {
+      id: Number(props.params.opportunityId),
+      is_deleted: false,
+      is_active: true,
+    },
+  });
+
+  if (!opportunity) {
+    return <div>Opportunity not found</div>;
+  }
   return (
     <div className="border p-4 m-4">
       <h1>{opportunity.title}</h1>
       <p className="p-4">{opportunity.details}</p>
       <p className="px-4">Location: {opportunity.location}</p>
       <p className="px-4">
-        {opportunity.start_date} - {opportunity.end_date}
+        {formatDateTime(opportunity.start_date)} -{" "}
+        {formatDateTime(opportunity.end_date)}
       </p>
       <div className="m-4 flex gap-4">
         <Button className="">Apply</Button>
